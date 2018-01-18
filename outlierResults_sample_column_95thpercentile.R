0e385f5e5921640268533f5d67546a728dc5fb60
Liam McKay <ltmckay@ucsc.edu>
2018-01-18 13:03:36 -0800

    installed and using new library ggpubr for combining graphs
    reason: easy to use, functions can be found in this link:
    http://www.sthda.com/english/articles/24-ggpubr-publication-ready-plots/81-ggplot2-easy-way-to-mix-multiple-graphs-on-the-same-page/

--- outlierResults_sample_column_95thpercentile.R
+++ outlierResults_sample_column_95thpercentile.R
@@ -1,6 +1,9 @@
 options(stringsAsFactors=FALSE) # for compatibile code between us
 
 library(tidyverse)
+library(ggpubr) # easy for putting graphs onto the same page (just use ggarrange(graph1, graph2, ncol = # of display
+                # columns, nrow = #row))
+
 
 setwd("~/Documents/UCSC/Junior/Treehouse/OutlierRNAseq_Treehouse_Repo/comp4.3_tert8.ckcc.outlier_results")
 
@@ -18,4 +21,30 @@ quantile(outlierResults$sample, c(0.95))
 # Should 0.0 TPM be considered in the data?
 # What is a cutoff point that is reliably accurate that the values are in the child's DNA for that gene
 
+# each percentile is a percentile of the sample in the sample column
 
+percentileOfEachSampleDf <- outlierResults %>%
+  group_by(sampleID) %>%
+  summarise(p95 = quantile(sample, c(0.95)))
+
+arrange(percentileOfEachSampleDf, p95) 
+
+# taking the histogram of a bad sample and comparing it to a good sample
+# TH01_0069_S01
+up_outlier_file_bad=list.files(, "outlier_results_TH01_0069_S01")
+
+bad_sample<-lapply(up_outlier_file_bad, function(x) {
+  read_tsv(x, col_types=cols()) %>%
+    add_column(sampleID=gsub("outlier_results_TH01_0069_S01", "", x))
+}) 	%>%
+  bind_rows()
+percentileOfEachSampleDf_bad_sample <- bad_sample %>%
+  group_by(sample) %>%
+  summarise(p95 = quantile(sample, c(0.95)))
+
+p1 <- ggplot(percentileOfEachSampleDf_bad_sample, aes(p95)) + geom_histogram()
+p2 <- ggplot(percentileOfEachSampleDf, aes(p95)) + geom_histogram()
+
+ggarrange(p1, p2, 
+          labels = c("A", "B"),
+          ncol = 2)
